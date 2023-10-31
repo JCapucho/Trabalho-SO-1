8db5da6c062dd66888ad9f5e10d2a0c2e6794cb0
João Capucho <jcapucho7@gmail.com>
2023-10-31 15:30:45 +0000

    spacerate: cleanup and handle spaces in filenames

--- spacerate.sh
+++ spacerate.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # BIBLIOGRAFIA
 # https://tldp.org/LDP/abs/html/tests.html
-set -eu
 PROGRAM_NAME="$0"
 
 NAME_SORT=false
@@ -14,7 +13,7 @@ help() {
 		>&2 echo "Error: $1"
 	fi
 
-	>&2 echo "Usage: $PROGRAM_NAME [options] <file1> <file2> "
+	>&2 echo "Usage: $PROGRAM_NAME [options] <old> <new> "
 	>&2 printf "\n"
 	>&2 echo "Options:"
 	>&2 echo -e "\t-h:\tShows this message"
@@ -60,22 +59,15 @@ while getopts ":hral:" o; do
 			;;
     esac
 done
-OPTIONS="${@:1:OPTIND-1}"
 shift $((OPTIND-1))
 
 if [ $# -lt 2 ]; then
-    help "Missing arguments"
-fi
-
-if [ $# -gt 2 ]; then
-		help "Too many arguments"
-fi
-
-if [ ! -f "$1" ]; then
+	help "Missing arguments"
+elif [ $# -gt 2 ]; then
+	help "Too many arguments"
+elif [ ! -f "$1" ]; then
     help "File \`$1\` does not exist"
-fi
-
-if [ ! -f "$2" ]; then
+elif [ ! -f "$2" ]; then
     help "File \`$2\` does not exist"
 fi
 
@@ -93,45 +85,51 @@ fi
 declare -A SPACECHECK_NEWEST
 declare -A SPACECHECK_OLDEST
 
-NEWESTFILE=""
-OLDESTFILE=""
+NEWESTFILE="$2"
+OLDESTFILE="$1"
 
-if [ $(head "$1" | awk '{print $3}') -ge $(head "$2" | awk '{print $3}') ]; then
-	NEWESTFILE="$1"
-	OLDESTFILE="$2"
-else
-	NEWESTFILE="$2"
-	OLDESTFILE="$1"
-fi
+# if [ $(head "$1" | awk '{print $3}') -ge $(head "$2" | awk '{print $3}') ]; then
+# 	NEWESTFILE="$1"
+# 	OLDESTFILE="$2"
+# else
+# 	NEWESTFILE="$2"
+# 	OLDESTFILE="$1"
+# fi
 
 while IFS=$'\n' read -r line; do
-  SPACECHECK_NEWEST[$(echo $line | awk '{print $2}')]=$(echo $line | awk '{print $1}')
+	size=$(echo $line | cut -d ' ' -f1)
+	name=$(echo $line | cut -d ' ' -f2-)
+	SPACECHECK_NEWEST["$name"]="$size"
 done < <(tail -n +2 "$NEWESTFILE")
 
 while IFS=$'\n' read -r line; do
-  SPACECHECK_OLDEST[$(echo $line | awk '{print $2}')]=$(echo $line | awk '{print $1}')
+	size=$(echo $line | cut -d ' ' -f1)
+	name=$(echo $line | cut -d ' ' -f2-)
+  	SPACECHECK_OLDEST["$name"]="$size"
 done < <(tail -n +2 "$OLDESTFILE")
 
 declare -A DIRECTORIES
 
 for key in "${!SPACECHECK_NEWEST[@]}"; do
-	DIRECTORIES[$key]=1
+	DIRECTORIES["$key"]=1
 done
 
 for key in "${!SPACECHECK_OLDEST[@]}"; do
-	DIRECTORIES[$key]=1
+	DIRECTORIES["$key"]=1
 done
-
 
 echo SIZE NAME
 
 for key in "${!DIRECTORIES[@]}"; do
-	if [ -z "${SPACECHECK_OLDEST[$key]+x}" ]; then
-		echo -e "${SPACECHECK_NEWEST[$key]}\t$key NEW"
-  elif [ -z "${SPACECHECK_NEWEST[$key]+x}" ]; then
-		echo -e "-${SPACECHECK_OLDEST[$key]}\t$key REMOVED"
-  else
-		echo -e "$((${SPACECHECK_NEWEST[$key]} - ${SPACECHECK_OLDEST[$key]}))\t$key"
+	old_size="${SPACECHECK_OLDEST[$key]}"
+	new_size="${SPACECHECK_NEWEST[$key]}"
+	if [ -z "$old_size" ]; then
+		echo -e "$new_size\t$key\tNEW"
+  	elif [ -z "$new_size" ]; then
+		echo -e "-$old_size\t$key\tREMOVED"
+  	else
+		diff=$((new_size - old_size))
+		echo -e "$diff\t$key"
 	fi
 done | \
 # NOTE: Sorting with `en_US.UTF-8` uses the unicode collation algorithm
